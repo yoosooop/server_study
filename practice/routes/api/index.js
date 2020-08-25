d8324a99ceee1a04b62939e96e0144b3640afb97
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-08-26 00:24:41 +0900

    finish 2nd seminar

---
+++ practice/routes/api/index.js
@@ -0,0 +1,15 @@
+//router() , express모듈 불러오기
+var express = require('express');
+var router = express.Router();
+
+//get으로 api 요청이 들어온다면 아래 로직 실행
+router.get('/', (req, res) => {
+    const result = {
+        status: 300,
+        message: "api~!"
+    }
+    res.status(200).send(result);
+})
+
+//생성한 router 객체를 모듈로 반환
+module.exports = router;
--- practice/routes/index.js
+++ practice/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+//생성한 폴더의 index에 접근하기 위해서
+router.use('/api', require('./api'))
+router.use('/blog', require('./blog'))
 
 module.exports = router;
